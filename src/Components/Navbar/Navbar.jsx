bdf57f3ed83495876f30d6920ab723cbda3541b1
freelanceradnan <reactorbro722@gmail.com>
2026-06-07 18:04:32 +0600

    spacility section added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -37,8 +37,8 @@ const Navbar = () => {
         <img src={assets.dropdown_icon} alt="" className='w-2.5'/>
         <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block '>
             <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
-            <p className='hover:text-black cursor-pointer'>My Profile</p>
-            <p className='hover:text-black cursor-pointer'>My Appointments</p>
+            <p className='hover:text-black cursor-pointer' onClick={()=>navigate('my-profile')}>My Profile</p>
+            <p className='hover:text-black cursor-pointer' onClick={()=>navigate('my-appointment')}>My Appointments</p>
             <p className='hover:text-black cursor-pointer' onClick={()=>setToken(false)}>Logout</p>
         </div>
         </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from '../../Components/Header/Header';
+import SpecialityMenu from '../../Components/SpecialityMenu/SpecialityMenu';
 
 const Home = () => {
     return (
         <div>
-           
+        <Header/>
+        <SpecialityMenu/>
         </div>
     );
 };
